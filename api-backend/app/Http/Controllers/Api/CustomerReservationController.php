589c342f8510e4ec71eaaf37e81e554efd77a270
Teodor469 <lifetroll6@gmail.com>
2024-11-08 15:22:12 +0200

    created index and store function in the customer reservation controller and resources.

--- api-backend/app/Http/Controllers/Api/CustomerReservationController.php
+++ api-backend/app/Http/Controllers/Api/CustomerReservationController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ReservationResource;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerReservationController extends Controller
 {
@@ -12,16 +14,35 @@ class CustomerReservationController extends Controller
     {
         $reservations = Reservation::get();
 
-        if ($reservations) {
-
+        if ($reservations->count() > 0) {
+            return ReservationResource::collection($reservations);
         } else {
             return response()->json(['message' => 'No record available'], 200);
         }
+
+        // This only returns one of the tables information (reservation_dates). In this table only the date is being displayed. We need to also return the table with the names.
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $validator = Validator::make($request->all(),[
+            'date' => 'required|date',
+            'time' => 'required|date_format:H:i',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'All fields are mandatory',
+                'error' => $validator->messages(),
+            ], 422);
+        }
+
+        $reservation = Reservation::create($validator->validated());
+
+        return response()->json([
+            'message' => 'Reservation created successfully',
+            'data' => new ReservationResource($reservation),
+        ], 200);
     }
 
     public function show()
